7c353c78ba96f0fc510c391d85539478193a6375
RedRyanD <redryand@gmail.com>
2021-07-27 10:43:37 +1000

    update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
+#include<limits>
+#include <vector>
+#include <sstream>
+#include<numeric>
+#include <ctime>
+#include <cmath>
+#include <fstream>
+
+
 
 int main()
 {
+
+    std::ofstream writeToFile;
+    std::ifstream readFromFile;
+    std::string txtToWrite = "";
+    std::string txtFromFile = "";
+
+    /*
+    writeToFile.open("test.txt", std::ios_base::out | std::ios_base::trunc);
+    if(writeToFile.is_open()){
+        writeToFile << "Beginning of File \n";
+        std::cout << "Enter data to write: ";
+        std::getline(std::cin, txtToWrite);
+        writeToFile << txtToWrite;
+        writeToFile.close();
+    }
+    */
+
+   readFromFile.open("test.txt", std::ios_base::in);
+   if(readFromFile.is_open()){
+       while (readFromFile.good())
+       {
+           std::getline(readFromFile, txtFromFile);
+           std::cout << txtFromFile << "\n";
+       }
+       readFromFile.close();
+   }
     
     return 0;
 }
